9b893ea2e0e4fb57d868b7c49d04379c7cf1ec89
Hkc-1673 <habibkc71@gmail.com>
2020-12-20 02:00:07 +0200

    add build files

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ require("dotenv").config();
 app.use(express.json());
 app.use("/api", router);
 
+//production
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+  }
+
+
 // connecting DB
 
 connectDB();
